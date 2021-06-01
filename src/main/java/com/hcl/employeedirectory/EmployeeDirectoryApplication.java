4ac03312ad33e5937f9a2a21046b22d6bc3ae3d2
SindhujaCheemala <84894665+SindhujaCheemala@users.noreply.github.com>
2021-06-01 21:07:33 +0530

    Update EmployeeDirectoryApplication.java

--- src/main/java/com/hcl/employeedirectory/EmployeeDirectoryApplication.java
+++ src/main/java/com/hcl/employeedirectory/EmployeeDirectoryApplication.java
@@ -2,12 +2,18 @@ package com.hcl.employeedirectory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeDirectoryApplication {
+public class EmployeeDirectoryApplication extends  SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(EmployeeDirectoryApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeDirectoryApplication.class, args);
 	}
+	
 
 }
